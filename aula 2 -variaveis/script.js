42b485ea9ad9c23372e5f76ed4e6b4f11eae4b81
Carlacorreia9 <carla9limasouza@gmail.com>
2023-02-28 13:42:22 -0300

    aula sitaxe javascipt

---
+++ aula 2 -variaveis/script.js
@@ -0,0 +1,15 @@
+/* var VouF = false;
+console.log( typeof VouF);
+
+// number
+/* var numeroQualquer = 1;
+console.log(typeof numeroQualquer);
+
+// string
+/* var nome = 'carla';
+console.log(typeof (nome));
+
+//
+/* var funcao = function() {}
+console.log(typeof funcao);
+
